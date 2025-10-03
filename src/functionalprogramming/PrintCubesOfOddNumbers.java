6143c9d831534397ca544322a6dfd6ed3645c77c
Yashdeep <ramawatyashdeep@gmail.com>
2025-10-03 21:52:15 +0530

    print cubes of only odd numbers

---
+++ src/functionalprogramming/PrintCubesOfOddNumbers.java
@@ -0,0 +1,19 @@
+package functionalprogramming;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
+public class PrintCubesOfOddNumbers {
+
+    public static void getOddNumbersAndCubeThem(List<Integer> numbers) {
+        numbers.stream()
+                .filter(number -> (number & 1) == 1)
+                .map(number -> number * number * number)
+                .forEach(System.out::println);
+    }
+
+    public static void main(String[] args) {
+        getOddNumbersAndCubeThem(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9 , 10));
+    }
+}
