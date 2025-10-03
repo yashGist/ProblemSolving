5a3bc7ceb2f213ddd28533affc5b88a189b784ee
Yashdeep <ramawatyashdeep@gmail.com>
2025-10-03 20:03:46 +0530

    Even or odd via fucntional prog

---
+++ src/functionalprogramming/PrintEvenInList.java
@@ -0,0 +1,27 @@
+package functionalprogramming;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class PrintEvenInList {
+    public static void printEven(List<Integer> numbers) {
+        numbers
+                .stream()
+                .filter(number -> (number & 1) == 0)
+                .forEach(System.out::println);
+    }
+
+    public static void even() {
+        int num = 52;
+        if ((num & 1) == 0) {
+            System.out.println("even");
+        } else {
+            System.out.println("odd");
+        }
+    }
+
+    public static void main(String[] args) {
+        printEven(Arrays.asList(12, 30, 33, 71, 25, 265, 59, 85));
+        //even();
+    }
+}
